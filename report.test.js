474722706f734bdec6fc4e831819ea25da78e100
HRSH455 <132004507+HRSH455@users.noreply.github.com>
2023-08-07 16:06:32 +0530

    Update report.test.js

--- report.test.js
+++ report.test.js
@@ -5,21 +5,16 @@ test('sortPages', () => {
   const input = {
     url1: 5,
     url2: 1,
-    url3: 3,
-    url4: 10,
-    url5: 7 
+    url3: 3
   }
   const actual = sortPages(input)
   const expected = [
-    [ 'url4', 10 ],
-    [ 'url5', 7 ],
     [ 'url1', 5 ],
     [ 'url3', 3 ],
     [ 'url2', 1 ]
   ]
   expect(actual).toEqual(expected)
 })
-
 test('sortPages null case', () => {
   const input = {}
   const actual = sortPages(input)
