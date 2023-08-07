861520c35eebd14ca160690c223e25900f246d46
HRSH455 <132004507+HRSH455@users.noreply.github.com>
2023-08-07 16:15:08 +0530

    Updated crawl.test.js

--- crawl.test.js
+++ crawl.test.js
@@ -2,60 +2,60 @@ const { normalizeURL, getURLsFromHTML } = require('./crawl.js')
 const { test, expect } = require('@jest/globals')
 
 test('normalizeURL protocol', () => {
-  const input = 'https://blog.boot.dev/path'
+  const input = 'https://www.geeksforgeeks.org/nodejs/'
   const actual = normalizeURL(input)
-  const expected = 'blog.boot.dev/path'
+  const expected = 'geeksforgeeks.org/nodejs/'
   expect(actual).toEqual(expected)
 })
 
 test('normalizeURL slash', () => {
-  const input = 'https://blog.boot.dev/path/'
+  const input = 'https://www.geeksforgeeks.org/nodejs/'
   const actual = normalizeURL(input)
-  const expected = 'blog.boot.dev/path'
+  const expected = 'geeksforgeeks.org/nodejs'
   expect(actual).toEqual(expected)
 })
 
 test('normalizeURL capitals', () => {
-  const input = 'https://BLOG.boot.dev/path'
+  const input = 'https://www.GEEKSFORGEEKS.org/nodejs/'
   const actual = normalizeURL(input)
-  const expected = 'blog.boot.dev/path'
+  const expected = 'geeksforgeeks.org/nodejs/'
   expect(actual).toEqual(expected)
 })
 
 test('normalizeURL http', () => {
-  const input = 'http://BLOG.boot.dev/path'
+  const input = 'https://www.GEEKSFORGEEKS.org/nodejs/'
   const actual = normalizeURL(input)
-  const expected = 'blog.boot.dev/path'
+  const expected = 'geeksforgeeks.org/nodejs/'
   expect(actual).toEqual(expected)
 })
 
 test('getURLsFromHTML absolute', () => {
-  const inputURL = 'https://blog.boot.dev'
-  const inputBody = '<html><body><a href="https://blog.boot.dev"><span>Boot.dev></span></a></body></html>'
+  const inputURL = 'https://www.geeksforgeeks.org/nodejs/'
+  const inputBody = '<html><body><a href="https://www.geeksforgeeks.org/nodejs/"><span>geeksforgeeks></span></a></body></html>'
   const actual = getURLsFromHTML(inputBody, inputURL)
-  const expected = [ 'https://blog.boot.dev/' ]
+  const expected = [ 'https://www.geeksforgeeks.org/nodejs/' ]
   expect(actual).toEqual(expected)
 })
 
 test('getURLsFromHTML relative', () => {
-  const inputURL = 'https://blog.boot.dev'
-  const inputBody = '<html><body><a href="/path/one"><span>Boot.dev></span></a></body></html>'
+  const inputURL = 'https://www.geeksforgeeks.org/nodejs/'
+  const inputBody = '<html><body><a href="/Ref/one"><span>geeksforgeeks></span></a></body></html>'
   const actual = getURLsFromHTML(inputBody, inputURL)
-  const expected = [ 'https://blog.boot.dev/path/one' ]
+  const expected = [ 'https://www.geeksforgeeks.org/nodejs//Ref/one' ]
   expect(actual).toEqual(expected)
 })
 
 test('getURLsFromHTML both', () => {
-  const inputURL = 'https://blog.boot.dev'
-  const inputBody = '<html><body><a href="/path/one"><span>Boot.dev></span></a><a href="https://other.com/path/one"><span>Boot.dev></span></a></body></html>'
+  const inputURL = 'https://www.geeksforgeeks.org/nodejs/'
+  const inputBody = '<html><body><a href="/Ref/one"><span>geekforgeeks></span></a><a href="https://other.com/path/one"><span>geeksforgeeks></span></a></body></html>'
   const actual = getURLsFromHTML(inputBody, inputURL)
-  const expected = [ 'https://blog.boot.dev/path/one', 'https://other.com/path/one' ]
+  const expected = [ 'https://www.geeksforgeeks.org/nodejs//Ref/one', 'https://other.com/Ref/one' ]
   expect(actual).toEqual(expected)
 })
 
 test('getURLsFromHTML handle error', () => {
-  const inputURL = 'https://blog.boot.dev'
-  const inputBody = '<html><body><a href="path/one"><span>Boot.dev></span></a></body></html>'
+  const inputURL = 'https://www.geeksforgeeks.org/nodejs/'
+  const inputBody = '<html><body><a href="path/one"><span>geeksforgeeks></span></a></body></html>'
   const actual = getURLsFromHTML(inputBody, inputURL)
   const expected = [ ]
   expect(actual).toEqual(expected)
