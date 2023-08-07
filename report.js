5413167838c1acfe6825b24e45be6859ee7ba2b8
HRSH455 <132004507+HRSH455@users.noreply.github.com>
2023-08-07 16:05:32 +0530

    Updated report.js

--- report.js
+++ report.js
@@ -1,9 +1,6 @@
-// printReport takes a dictionary of pages and prints them
-// to the console in a human-friendly way
 function printReport(pages){
-    console.log('==========')
     console.log('REPORT')
-    console.log('==========')
+    console.log('////////')
     const sortedPages = sortPages(pages)
     for (const sortedPage of sortedPages){
       const url = sortedPage[0]
@@ -13,18 +10,13 @@ function printReport(pages){
   }
   
   // sortPages sorts a dictionary of pages
-  // into a list of tuples (url, count)
-  // with the highest counts first in the list
   function sortPages(pages){
-    // 2D array where the
-    // inner array: [ url, count ]
     const pagesArr = Object.entries(pages)
     pagesArr.sort((pageA, pageB) => {
       return pageB[1] - pageA[1]
     })
     return pagesArr
   }
-  
   module.exports = {
     printReport,
     sortPages
